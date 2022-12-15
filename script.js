65f6c479d641e3dbe99dcbc0d7e5fd001db25547
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-12-15 07:59:13 +0100

    animation

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ const img = document.querySelector("#character")
 
 let x = 0
 let y = 0
+let pos = 0
+let frameCounter = 0
 
 function animationLoop () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    ctx.drawImage(img, 0, 0, 32, 32, x, y, canvas.width, canvas.height)
+    ctx.drawImage(img, pos * 32, 0, 32, 32, x, y, canvas.width, canvas.height)
+
+    frameCounter++
+    if (frameCounter >= 10) {
+        pos++
+        if (pos >= 3) {
+            pos = 0
+        }
+        frameCounter = 0
+    }
 
     window.requestAnimationFrame(animationLoop)
 }
