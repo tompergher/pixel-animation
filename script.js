4ff70a21f2beea151828bf63c920da21518ee88a
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-01-23 07:42:39 +0100

    created game

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-import { Player } from "./game_objects.js"
-import Map from "./map.js"
+import Game from "./game.js"
 
-const tileSize = 32
-const canvas = document.querySelector("#canvas")
-canvas.width = 10 * tileSize
-canvas.height = 15 * tileSize
-const ctx = canvas.getContext("2d")
-ctx.imageSmoothingEnabled = false
-
-let map = null
-let player = null
+let game = null
 
 function animationLoop() {
-  player.update()
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  map.drawMap(ctx)
-  player.draw(ctx)
+  game.gameLoop()
 
   window.requestAnimationFrame(animationLoop)
 }
 
 function main() {
-  map = new Map("map.txt")
-  player = new Player(4, 5)
+  game = new Game()
   window.requestAnimationFrame(animationLoop)
 }
 
