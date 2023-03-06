a3ecb8ae492d0a8c1b3e22932402fa89d9d5ee54
TOP <e227599@sbl.ch>
2023-03-06 09:06:58 +0100

    Background, Format, Grösse

--- src/game.js
+++ src/game.js
@@ -12,8 +12,8 @@ export default class Game {
   constructor() {
     this.tileSize = 32
     this.canvas = document.querySelector("#canvas")
-    this.canvas.width = 10 * this.tileSize
-    this.canvas.height = 15 * this.tileSize
+    this.canvas.width = 19 * this.tileSize
+    this.canvas.height = 11 * this.tileSize
     this.ctx = this.canvas.getContext("2d")
     this.ctx.imageSmoothingEnabled = false
 
