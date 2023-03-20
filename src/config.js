523342126008cfc609105e8d0892a5373bc45cfc
Alea Ducret <e227578@sbl.ch>
2023-03-20 08:55:39 +0100

    Jump Pfeiltasten repariert

--- src/config.js
+++ src/config.js
@@ -2,15 +2,13 @@ import Game from "./game.js"
 
 const config = {
   "keys": {
-    "KeyW": function() { Game.player.move("up")},
+    "KeyW": function() { Game.player.jump()},
     "KeyA": function() { Game.player.move("left")},
-    "KeyS": function() { Game.player.move("down")},
     "KeyD": function() { Game.player.move("right")},
     "Space": function() {Game.player.jump() },
-    "ArrowUp": function() { Game.player2.move("up")},
-    "ArrowLeft": function() { Game.player2.move("left")},
-    "ArrowDown": function() { Game.player2.move("down")},
-    "ArrowRight": function() { Game.player2.move("right")},
+    "ArrowUp": function() { Game.player.jump()},
+    "ArrowLeft": function() { Game.player.move("left")},
+    "ArrowRight": function() { Game.player.move("right")},
   }
 }
 
