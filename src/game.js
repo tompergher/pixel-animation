433ec8c8bed1b6f44f13b91f052032a33a55964d
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-03-30 14:04:59 +0200

    Added pause

--- src/game.js
+++ src/game.js
@@ -18,8 +18,10 @@ export default class Game {
   static currentFrame = 0;
   static canvas = document.querySelector("#canvas")
   static tileSize = 32
+  static instance = null
 
   constructor() {
+    Game.instance = this
     Game.canvas.width = 10 * Game.tileSize
     Game.canvas.height = 15 * Game.tileSize
     this.ctx = Game.canvas.getContext("2d")
@@ -30,6 +32,9 @@ export default class Game {
     Game.loadMap("maps/map-01.txt")
 
     this.camera = new Camera(this)
+
+    document.querySelector("#game-start").addEventListener("click", () => { Game.start() })
+    document.querySelector("#game-pause").addEventListener("click", () => { Game.pause() })
 
     Game.running = false
     window.requestAnimationFrame(this.gameLoop.bind(this))
@@ -43,6 +48,8 @@ export default class Game {
    */
   static start() {
     Game.running = true
+    document.querySelector("body").classList.remove("paused")
+    window.requestAnimationFrame(Game.instance.gameLoop.bind(Game.instance))
   }
 
   /**
@@ -55,6 +62,7 @@ export default class Game {
    * `start()` aufgerufen werden.
    */
   static pause() {
+    document.querySelector("body").classList.add("paused")
     Game.running = false
   }
 
