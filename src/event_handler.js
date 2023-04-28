096c7d8fcc322da8a27aed6460efe128c62e4d40
Alea Ducret <e227578@sbl.ch>
2023-04-28 13:15:40 +0200

    fix space

--- src/event_handler.js
+++ src/event_handler.js
@@ -11,7 +11,7 @@ export default class InputHandler {
 
   constructor() {
     // Setup Eventlisteners
-    window.onkeydown = (ev) => {InputHandler.events.add(ev.code)}
+    window.onkeydown = (ev) => {InputHandler.events.add(ev.code); return false}
     window.onkeyup = (ev) => {InputHandler.events.delete(ev.code)}
     Object.entries(config["keys"]).forEach(([key, callback]) => {
       new Command(key, callback)
